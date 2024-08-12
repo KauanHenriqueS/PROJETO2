e577720c38443e834e144b9ec6caf101500172e8
KauanHenriqueS <00001098845821sp@al.educacao.sp.gov.br>
2024-08-12 17:23:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let posicaoHorizontal; // x
 let posicaoVertical; // y
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1400, 900);
    background(color(100,0,0));
   cor = color(random(0,255), random(0,255), random(0,255));
   posicaoHorizontal = 200;
